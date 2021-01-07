041b03b9a317d67234753aeb13014650dc0b9236
Kamil Kamiński <kkaminski9311@gmail.com>
2021-01-07 22:39:19 +0100

    Extract content info from git

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -13,6 +13,27 @@ export const onCreateNode: GatsbyNode["onCreateNode"] = async ({
   if (node.internal.type !== "File") return;
   if (node.sourceInstanceName !== "posts") return;
 
-  const g = await execPromisified(`git log ${node.absolutePath}`);
-  console.log("g", g);
+  try {
+    const g = await execPromisified(
+      `git log --pretty=format:%as%n ${node.absolutePath}`
+    );
+    const splitted = g.stdout.split("\n");
+    const creationDate = splitted[0].split("-").reverse().join(".");
+    const lastEdit = splitted[splitted.length - 1]
+      .split("-")
+      .reverse()
+      .join(".");
+
+    createNodeField({ node, name: "postDate", value: creationDate });
+    createNodeField({
+      node,
+      name: "lastEditDate",
+      value: lastEdit === creationDate ? "" : lastEdit,
+    });
+  } catch (e) {
+    console.error(e);
+
+    createNodeField({ node, name: "postDate", value: "2021-01-01" });
+    createNodeField({ node, name: "lastEditDate", value: "2021-01-01" });
+  }
 };
