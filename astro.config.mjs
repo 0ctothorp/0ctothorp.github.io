d64f66a0d09788bad48f42c4eec97b36a9ed0cec
Kamil Kamiński <kkaminski9311@gmail.com>
2023-02-26 14:26:53 +0100

    making player popo out from the header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import image from "@astrojs/image";
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), image()]
+  site: 'https://kamilkaminski.dev',
+  integrations: [mdx(), sitemap(), tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  })]
 });
