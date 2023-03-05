1ced655e46d895a3c72a7a280bad2a7383bdbe13
Kamil Kamiński <kkaminski9311@gmail.com>
2023-03-05 22:23:20 +0100

    play with the play button; fix z-index with email in footer

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,29 @@
 export const wait = (ms: number) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+
+export function debounce<T extends Array<any>, R>(
+  fn: (...args: T) => void,
+  time: number
+) {
+  let timeoutId: number;
+  return (...args: T) => {
+    if (timeoutId) clearTimeout(timeoutId);
+    timeoutId = window.setTimeout(() => fn(...args), time);
+  };
+}
+
+export function throttle(callback: () => void, time: number) {
+  let queued = false;
+
+  return () => {
+    if (queued) {
+      return;
+    }
+
+    queued = true;
+    setTimeout(() => {
+      callback();
+      queued = false;
+    }, time);
+  };
+}
