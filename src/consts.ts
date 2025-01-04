5e1ca8ca5349a967fb5b6b29210095d5862c0a9d
Kamil Kamiński <kkaminski9311@gmail.com>
2025-01-04 22:08:59 +0100

    remove music video popup and header; change music link; change title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "0ctothorp - My own piece of the internet";
+export const SITE_TITLE = "Kamil Kamiński - web developer's digital garden";
 export const SITE_DESCRIPTION = "Welcome to my personal website";
