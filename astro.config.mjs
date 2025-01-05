1aa3735a420394bf2655f2ba03dbe5c3a3d8d7c9
Kamil Kamiński <kkaminski9311@gmail.com>
2025-01-05 02:00:51 +0100

    fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,15 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 
 export default defineConfig({
   site: "https://kamilkaminski.dev",
   integrations: [
-    mdx(),
     sitemap(),
     tailwind({
       config: {
         applyBaseStyles: false,
       },
-    }),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
     }),
   ],
 });
