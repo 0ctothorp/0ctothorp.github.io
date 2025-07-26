bd67ce7942b6b10ad44de9b314fca83398e717a5
Kamil Kamiński <kkaminski9311@gmail.com>
2025-07-27 00:26:06 +0200

    feat: make ToC foldable instead of hiding completely; Turn commands into headers in git cheatsheet;
    - installed lucide-astro
    - started to define colors in tailwind theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				bg: '#0a0908',
+			}
+		},
 	},
 	plugins: [],
 }
